41aee495ffda74f353f454792462b1a42bc79964
MehvixDesktop <mehvix@protonmail.com>
2019-03-24 02:20:29 -0500

    Renamed files

--- copy.py
+++ copy.py
@@ -46,13 +46,13 @@ def copy(files: list, folders: list, location: str, game: str):
             print("\n[** {} - ERROR] {}\n".format(game, e))
 
 
-# Age of Empires II
+# ========================== Age of Empires II ==========================
 location = drive + r"Program Files (x86)\Steam\steamapps\common\Age2HD\Profiles"
 configFiles = ["player0.hki"]  # Maybe Steam workshop files / mods?
 copy(configFiles, [], location, "Age2HD")
 
 
-# TF2
+# ========================== TF2 ==========================
 location = drive + r"Program Files (x86)\Steam\steamapps\common\Team Fortress 2\tf\cfg"
 configFiles = ["autoexec.cfg", "server_blacklist.txt", "config.cfg", "settings.cfg","custom.cfg", "gfx.cfg", "network.cfg", "binds.cfg",
                "demoman.cfg", "engineer.cfg", "heavyweapons.cfg", "medic.cfg", "sniper.cfg", "spy.cfg", "pyro.cfg", "scout.cfg", "soldier.cfg"]
@@ -60,14 +60,18 @@ configFolders = [r"tweaks"]
 copy(configFiles, configFolders, location, "Team Fortress 2")
 
 
-# Apex
+# ========================== Apex ==========================
 # Todo: Get nvidia settings file
 location = drive + r"Users\{}\Saved Games\Respawn\Apex".format(user)
 configFolders = [r"local", r"profile"]  # Maybe Steam workshop files / mods?
 copy([], configFolders, location, "Apex")
 
+location = drive + r"Program Files (x86)\Origin Games\Apex\cfg"
+configFiles = ["autoexec.cfg"]  # Maybe Steam workshop files / mods?
+copy(configFiles, [], location, "Apex")
 
-# Quake Live
+
+# ========================== Quake Live ==========================
 steamid = (os.listdir(drive + r"Program Files (x86)\Steam\steamapps\common\Quake Live"))[0]
 
 location = drive + r"Program Files (x86)\Steam\steamapps\common\Quake Live\{}\baseq3".format(steamid)
